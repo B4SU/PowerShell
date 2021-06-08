bd33307020f3c2f0d012e4cd1f3ef26586bfc32b
B4SU <hexact9@gmail.com>
2021-06-08 13:21:17 +0530

    Get-AuditPol created

---
+++ Get-AuditPol.ps1
@@ -0,0 +1,3 @@
+Clear
+mkdir $env:USERPROFILE\B4SU -ErrorAction SilentlyContinue
+auditpol /get /category:* | Out-File -FilePath "$env:USERPROFILE\B4SU\$env:COMPUTERNAME-$(Get-Date -Format "yyyyMMdd-hhmm").txt"
