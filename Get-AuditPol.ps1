35f92217e6c6ca460e5bc75d144a6237e33bc193
B4SU <67707918+B4SU@users.noreply.github.com>
2022-06-13 17:23:04 +0530

    Delete Get-AuditPol.ps1

--- Get-AuditPol.ps1
+++
@@ -1,4 +0,0 @@
-Clear
-mkdir $env:USERPROFILE\B4SU -ErrorAction SilentlyContinue
-$X = Get-Service | Select-Object -property Name,DisplayName,ServiceName,Status,StartType
-Add-Content "$env:USERPROFILE\B4SU\$env:COMPUTERNAME-$(Get-Date -Format "yyyyMMdd-hhmm").csv" $X
